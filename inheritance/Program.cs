0049763b92fee134543321b6b8ef254a3a9a9b59
Siranjeevi S <siranjeevicad01@brightskills.com>
2024-02-21 18:03:18 +0530

    created a inheritance

---
+++ inheritance/Program.cs
@@ -0,0 +1,23 @@
+﻿class Saran
+{
+    public saran(){
+        Console.WriteLine("Did you Know Me...,I'm Saran");
+    }
+    public  string shaddow(string shaddowword){
+        return "na iruttulaye vazhravan..." + shaddowword;
+    }
+
+}
+    class Ramcharan : saran
+    {
+        public ramcharan(sring name){
+            Console.WriteLine("shaddow");       
+        }
+    }
+
+
+    class Punch{
+        static void Main(string [] args){
+            Ramcharan charan=new Ramcharan("Enaku irutta paathu bayam ill");
+        }
+    }
